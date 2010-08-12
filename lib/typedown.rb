bbeee468663a24b7cf7d052e65def28e706746da
wrimle <rmyrland@gmail.com>
2010-08-13 00:42:15 +0200

    Creating rubygem structure with newgen, updating README.rdoc and settings

--- lib/typedown.rb
+++ lib/typedown.rb
@@ -1,63 +1,9 @@
-require 'rubygems'
-#require 'kramdown'
-require 'bluecloth'
+$:.unshift(File.dirname(__FILE__)) unless
+  $:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
+
+require 'typedown/document'
 
 module Typedown
-  VERSION = "0.1"
-
-  class Document < String
-    def initialize text = nil
-      super ""
-      self << text if text
-    end
-
-    def to_markdown
-      text = self
-
-      # Translate '! headings' to '# headings'
-      text.gsub!(/^! /, "# ")
-      text.gsub!(/^!! /, "## ")
-      text.gsub!(/^!!! /, "### ")
-      text.gsub!(/^!!!! /, "#### ")
-      text.gsub!(/^!!!!! /, "##### ")
-      text.gsub!(/^!!!!!! /, "###### ")
-
-      # Dialogue dashes
-      text.gsub!(/^-\.? /, "&#8213; ")
-
-      # Insert placeholders around lead in
-      text.gsub!(/^\. (( *[^\n].+\n)*)/, "! x!\\1!x-!\n")
-      # Remove placeholders while using them to remove whitespace
-      text.gsub!(/^! x!\s*/, "**")
-      text.gsub!(/\s*!x-!$/, "**")
-
-
-      # Insert placeholders around lead in
-      text.gsub!(/^\/\/\. (( *[^\n].+\n)*)/, "! x!\\1!x-!\n")
-      # Remove placeholders while using them to remove whitespace
-      text.gsub!(/^! x!\s*/, "**")
-      text.gsub!(/\s*!x-!$/, "**")
-
-
-      # Insert placeholders around lead in
-      text.gsub!(/^\/\. (( *[^\n].+\n)*)/, "! x!\\1!x-!\n")
-      # Remove placeholders while using them to remove whitespace
-      text.gsub!(/^! x!\s*/, "*")
-      text.gsub!(/\s*!x-!$/, "*")
-
-      # Slash for _
-      text.gsub!(/(\s)\/(\S+)\/(\s)[\.\,\?\!]?/, "\\1_\\2_\\3")
-      text.gsub!(/(\s)\/\/(\S+)\/\/[\.\,\?\!]?(\s)/, "\\1__\\2__\\3")
-      text.gsub!(/(\s)\/\/\/(\S+)\/\/\/[\.\,\?\!]?(\s)/, "\\1___\\2___\\3")
-      text
-    end
-
-
-    def to_html
-      #kramdown = Kramdown::Document.new to_markdown
-      #kramdown.to_html
-      bluecloth = BlueCloth::new( to_markdown )
-      bluecloth.to_html
-    end
-  end
+  VERSION = '0.0.1'
 end
+
