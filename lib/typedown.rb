b1293f273c7550400c4c8576d860254eac5186aa
wrimle <rmyrland@gmail.com>
2010-08-12 23:17:55 +0200

    First checkin

---
+++ lib/typedown.rb
@@ -0,0 +1,63 @@
+require 'rubygems'
+#require 'kramdown'
+require 'bluecloth'
+
+module Typedown
+  VERSION = "0.1"
+
+  class Document < String
+    def initialize text = nil
+      super ""
+      self << text if text
+    end
+
+    def to_markdown
+      text = self
+
+      # Translate '! headings' to '# headings'
+      text.gsub!(/^! /, "# ")
+      text.gsub!(/^!! /, "## ")
+      text.gsub!(/^!!! /, "### ")
+      text.gsub!(/^!!!! /, "#### ")
+      text.gsub!(/^!!!!! /, "##### ")
+      text.gsub!(/^!!!!!! /, "###### ")
+
+      # Dialogue dashes
+      text.gsub!(/^-\.? /, "&#8213; ")
+
+      # Insert placeholders around lead in
+      text.gsub!(/^\. (( *[^\n].+\n)*)/, "! x!\\1!x-!\n")
+      # Remove placeholders while using them to remove whitespace
+      text.gsub!(/^! x!\s*/, "**")
+      text.gsub!(/\s*!x-!$/, "**")
+
+
+      # Insert placeholders around lead in
+      text.gsub!(/^\/\/\. (( *[^\n].+\n)*)/, "! x!\\1!x-!\n")
+      # Remove placeholders while using them to remove whitespace
+      text.gsub!(/^! x!\s*/, "**")
+      text.gsub!(/\s*!x-!$/, "**")
+
+
+      # Insert placeholders around lead in
+      text.gsub!(/^\/\. (( *[^\n].+\n)*)/, "! x!\\1!x-!\n")
+      # Remove placeholders while using them to remove whitespace
+      text.gsub!(/^! x!\s*/, "*")
+      text.gsub!(/\s*!x-!$/, "*")
+
+      # Slash for _
+      text.gsub!(/(\s)\/(\S+)\/(\s)[\.\,\?\!]?/, "\\1_\\2_\\3")
+      text.gsub!(/(\s)\/\/(\S+)\/\/[\.\,\?\!]?(\s)/, "\\1__\\2__\\3")
+      text.gsub!(/(\s)\/\/\/(\S+)\/\/\/[\.\,\?\!]?(\s)/, "\\1___\\2___\\3")
+      text
+    end
+
+
+    def to_html
+      #kramdown = Kramdown::Document.new to_markdown
+      #kramdown.to_html
+      bluecloth = BlueCloth::new( to_markdown )
+      bluecloth.to_html
+    end
+  end
+end
